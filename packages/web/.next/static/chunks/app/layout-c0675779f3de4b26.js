7eff56183bb340a58b1783bbd08b62332e08e299
Deepak <deepak@example.com>
2026-01-30 18:58:14 +0530

    chore(web): ensure .next artifacts are ignored

--- packages/web/.next/static/chunks/app/layout-c0675779f3de4b26.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6243:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3139,23))},3139:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[678,587,855,358],()=>s(6243)),_N_E=e.O()}]);
